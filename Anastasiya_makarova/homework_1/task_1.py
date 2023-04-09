8abf42e69969cf2c395ba3957448ab40bd9c20ea
miramkrv <kalista198933@gmail.com>
2023-04-09 16:37:52 +0300

    homework_1 task_1

---
+++ Anastasiya_makarova/homework_1/task_1.py
@@ -0,0 +1,18 @@
+var_int = 10
+var_float = 8.4
+var_str = "No"
+
+big_int = var_int * 3.5
+
+var_float -= 1
+
+var_int / var_float
+big_int / var_float
+
+var_str = "No" * 2 + "Yes" * 3
+
+print(var_int)
+print(var_float)
+print(var_str)
+print(big_int)
+
