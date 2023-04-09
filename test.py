2426925725af4b0c2391852d8471374b2ea71985
shwrk <razxca@icloud.com>
2023-04-09 18:49:47 +0300

    up

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ var_int = 10
 var_float = 8.4
 var_str = "No"
 big_int = var_int * 3.5
-var_float = var_float - 1
+var_float -= 1
 var_a = var_int / var_float
 var_b = big_int / var_float
-var_str = "NoNoYesYesYes"
+var_str = "No" * 2 + "Yes" * 3
 print(var_int, var_float, var_str, big_int, var_a, var_b)
