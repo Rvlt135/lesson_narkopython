7b03038843cd7552c820f71e8963a4566b82defe
nkolotygin <11ivt1@gmail.com>
2023-04-26 16:38:15 +0300

    nkolotygin_commit_hw2_dicts

---
+++ Nikita_Kolotygin/homework_2/3_hw2_dicts.py
@@ -0,0 +1,17 @@
+s_dict = {"1a":20, "1b":30, "1v":22, "2a": 23, "2b": 15}
+
+
+#count of 1v
+print(s_dict["1v"])
+
+#changes in school
+s_dict["1a"] = 23
+s_dict["2b"] = 18
+s_dict["1b"] = 28
+s_dict["3a"] = 28
+s_dict["3b"] = 33
+del s_dict["1v"]
+
+
+#full dict
+print(s_dict)
