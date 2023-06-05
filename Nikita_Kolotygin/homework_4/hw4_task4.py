622cddc3d9471950a95ac49267178d51e5f89998
nkolotygin <11ivt1@gmail.com>
2023-06-05 22:05:53 +0300

    nkolotygin_commit_hw4_4

---
+++ Nikita_Kolotygin/homework_4/hw4_task4.py
@@ -0,0 +1,18 @@
+
+
+def ceazer_crypt():
+    alfavit =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    smeshenie = int(input('Шаг шифровки: '))
+    message = input("Сообщение для шифровки: ").upper()
+    itog = ''
+    for i in message:
+        mesto = alfavit.find(i)
+        new_mesto = mesto + smeshenie
+        if i in alfavit:
+            itog += alfavit[new_mesto]
+        else:
+            itog += i
+    print (itog)
+    return itog
+
+ceazer_crypt()
